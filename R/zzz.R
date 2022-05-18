6380e05ade16a06a4f75db69e77f1a80ad4a3ea5
Chris Haid <christopher.haid@ideapublicschools.org>
2022-05-18 07:22:28 -0500

    updated start up messages to be more infomratinve

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,25 @@
 .onAttach <- function(...) {
 
-  packageStartupMessage("Gathering warehouse metadata")
+  packageStartupMessage(cli::rule(left = "Gathering warehouse metadata",
+                                  right = paste0("ideadata ", packageVersion("ideadata"))))
+
+  packageStartupMessage(paste(cli::symbol$info, "Checking credentials ..."))
+
+  creds <- get_creds()
+
+  kinit(creds$uid, creds$pwd)
   warehouse_meta_data <- get_warehouse_meta_data()
   e1<-parent.env(environment())
   assign("warehouse_meta_data", warehouse_meta_data, pos = "package:ideadata")
-  packageStartupMessage("Success: Warehouse metadata gathered!")
-  packageStartupMessage(glue::glue("{length(unique(warehouse_meta_data$table_name))} tables"))
-  packageStartupMessage(glue::glue("{length(unique(warehouse_meta_data$database_name))} databases"))
-  packageStartupMessage(glue::glue("{length(unique(warehouse_meta_data$server_name))} servers"))
+
+
+  if(length(unique(warehouse_meta_data$table_name))==0) stop(crayon::red(paste(cli::symbol$circle_cross, "There are 0 tables, which means there's a problem.  Check your VPN or Contact Chris!")))
+  packageStartupMessage(crayon::green(paste(cli::symbol$tick, "Success: Warehouse metadata gathered!")))
+  packageStartupMessage(paste(cli::symbol$info, "The warehouse currently houses:"))
+  packageStartupMessage(glue::glue("     {prettyNum(length(unique(warehouse_meta_data$table_name)), big.mark=',')} tables"))
+  packageStartupMessage(glue::glue("  in {prettyNum(length(unique(warehouse_meta_data$database_name)), big.mark=',')} databases"))
+  packageStartupMessage(glue::glue("  on {prettyNum(length(unique(warehouse_meta_data$server_name)), big.mark=',')} servers"))
+  packageStartupMessage(crayon::yellow(praise::praise("${EXCLAMATION}! ideadata and you are ${adverb} ${adjective}!")))
 
 
 }
